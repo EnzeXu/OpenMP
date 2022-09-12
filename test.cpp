47263151c5ec90036cbe93f4c7162c99083036de
EnzeXu <xue20@wfu.edu>
2022-09-12 18:20:40 -0400

    updates

--- test.cpp
+++ test.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 int main()
 {
+	omp_set_num_threads(2);
 #pragma omp parallel
 	{
-		cout << "Hello, world!" << endl;
+#pragma omp for
+		for(int i=0;i<4;i++)
+		 cout << omp_get_thread_num() << endl;
 	}
 }
 
